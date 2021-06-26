6e058fc1fb026fb702f19f4aa8e83f63739a0ac8
Justin <jbarabander@gmail.com>
2021-06-26 02:54:52 -0400

    Allow author comments

--- removeCheckmarkComments.js
+++ removeCheckmarkComments.js
@@ -5,17 +5,18 @@ const commentRequest = 'https://www.youtube.com/comment_service_ajax?action_get_
 (function() {
     const originalOpen = XMLHttpRequest.prototype.open;
     XMLHttpRequest.prototype.open = function(method, url, ...rest) {
-        console.log('too late');
         if (url.includes('/comment_service_ajax?action_get_comments')) {
             this.addEventListener('readystatechange', function(event) {
                 if ( this.readyState === 4 ) {
                    const parsedResponse = JSON.parse(event.target.responseText);
                    if(parsedResponse.response?.continuationContents?.itemSectionContinuation?.contents) {
-                    const onlyNonVerified = parsedResponse.response.continuationContents.itemSectionContinuation.contents.filter((content) => {
-                        return content?.commentThreadRenderer?.comment?.commentRenderer?.authorCommentBadge?.authorCommentBadgeRenderer?.iconTooltip !== "Verified";
+                    const onlyNonVerifiedOrAuthor = parsedResponse.response.continuationContents.itemSectionContinuation.contents.filter((content) => {
+                        const commentRenderer = content?.commentThreadRenderer?.comment?.commentRenderer;
+                        return commentRenderer?.authorIsChannelOwner 
+                        || commentRenderer?.authorCommentBadge?.authorCommentBadgeRenderer?.iconTooltip !== "Verified";
                     });
 
-                    parsedResponse.response.continuationContents.itemSectionContinuation.contents = onlyNonVerified;
+                    parsedResponse.response.continuationContents.itemSectionContinuation.contents = onlyNonVerifiedOrAuthor;
                 }
                    Object.defineProperty(this, 'response',     {writable: true});
                    Object.defineProperty(this, 'responseText', {writable: true});
